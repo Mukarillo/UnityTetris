1360adf48779bab203eb1eaae504f1a3156d7c32
Skota18855 <47341526+Skota18855@users.noreply.github.com>
2021-02-24 20:14:50 -0700

    Added a Zen/Endless Mode
    Added a mode that keeps going forever and every time the player would lose, it would reset the board but the score would remain.

--- Assets/Scripts/View/Screens/Score.cs
+++ Assets/Scripts/View/Screens/Score.cs
@@ -3,43 +3,52 @@ using DG.Tweening;
 using UnityEngine;
 
 public class Score : ScoreScreenType<Score>
-{   
-	public int PlayerScore{
-		get{
-			return mInternalPoints;
-		}
-	}
+{
+    public int PlayerScore
+    {
+        get
+        {
+            return mInternalPoints;
+        }
+    }
 
     private int mInternalPoints = 0;
 
     public void AddPoints(int points)
     {
-        mInternalPoints += points;
-		SetScoreText(mInternalPoints);
-		ShowScreen();
+        if (mInternalPoints + points < mInternalPoints) 
+        { 
+            mInternalPoints = int.MaxValue; 
+        }
+        else
+        {
+            mInternalPoints += points;
+        }
+        SetScoreText(mInternalPoints);
+        ShowScreen();
     }
 
     public void ResetScore()
     {
         mInternalPoints = 0;
-		SetScoreText(mInternalPoints);
+        SetScoreText(mInternalPoints);
     }
 
-	public override void ShowScreen(float timeToTween = 1f)
-	{
-		base.ShowScreen(timeToTween);
-		StopCoroutine(WaitAndHide());
-		StartCoroutine(WaitAndHide());
-	}
+    public override void ShowScreen(float timeToTween = 1f)
+    {
+        base.ShowScreen(timeToTween);
+        StopCoroutine(WaitAndHide());
+        StartCoroutine(WaitAndHide());
+    }
 
-	private IEnumerator WaitAndHide()
-	{
-		yield return new WaitForSeconds(2f);
-		HideScreen();
-	}
+    private IEnumerator WaitAndHide()
+    {
+        yield return new WaitForSeconds(2f);
+        HideScreen();
+    }
 
-	protected override void InternalAlphaScreen(float timeToTween, float alpha, TweenCallback callback)
-	{
-		base.InternalAlphaScreen(timeToTween, Mathf.Clamp(alpha, 0.2f, 1f), callback);
-	}
+    protected override void InternalAlphaScreen(float timeToTween, float alpha, TweenCallback callback)
+    {
+        base.InternalAlphaScreen(timeToTween, Mathf.Clamp(alpha, 0.2f, 1f), callback);
+    }
 }
